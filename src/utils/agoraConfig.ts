77b7019038cde04ddeff3d00b9c2e5d0d3b89bd3
pramod-12345 <102239873+pramod-12345@users.noreply.github.com>
2025-02-27 20:41:05 +0530

    Update agoraConfig.ts

--- src/utils/agoraConfig.ts
+++ src/utils/agoraConfig.ts
@@ -1,3 +1,4 @@
+// Add your Agora api keys and other keys here
 export const AGORA_APP_ID = "AGORA_APP_ID";
 export const AGORA_CHANNEL_NAME = "AGORA_CHANNEL_NAME"; // Set a channel name
 export const AGORA_TEMP_TOKEN = "AGORA_TEMP_TOKEN"; // Replace with your token
